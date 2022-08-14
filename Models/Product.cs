4425c6fde39feb7338fa5579f979d37cbfa8cd5f
Natce <natcelnieves@gmail.com>
2022-08-14 11:16:25 -0400

    Adding stuff

---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace Ecommercecsharp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+    }
+    public class Rating
+    {
+        public double rate { get; set; }
+        public int count { get; set; }
+    }
+
+    public class ProductAPI
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+        public Rating rating { get; set; }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Ecommercecsharp.Data;
+using Ecommercecsharp.Models;
+using Microsoft.EntityFrameworkCore;
+using Ecommercecsharp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<ProductDbContext>(options =>
+{
+    options.UseSqlite("Data Source = Products.db");
+});
+builder.Services.AddScoped<ProductServices>();
 
 var app = builder.Build();
 
